eb2426bc925cb0ecf2b295ffba1b4572c17415b5
Zach Leach <zleach@mac.com>
2024-12-03 12:28:15 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,41 +1,139 @@
-import React from 'react';
 import {
-  ChakraProvider,
-  Box,
-  Text,
+  Image,
+  Container,
+  Heading,
+  Stack,
   Link,
-  VStack,
+  Text,
+  OrderedList,
+  UnorderedList,
+  ListItem,
   Code,
-  Grid,
-  theme,
+  Divider,
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import React from 'react';
 
 function App() {
   return (
-    <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
+    <Container py={12} pb={36}>
+      <Stack spacing={6}>
+        <Image
+          src="/images/logo.svg"
+          alt="Pepperoni Planet Logo"
+          mx="auto"
+          my={12}
+          w="32"
+        />
+
+        <Heading size="sm">Design Engineer Take-home Project</Heading>
+        <Heading size="xl">Pepperoni Planet Pizza Configurator</Heading>
+        <Text fontSize="lg">
+          Imagine you are a design engineer at Pepperoni Planet, and you're
+          tasked with prototyping a single screen of the pizza ordering flow.
+          You've been provided colors, a UI library called Chakra UI, and some
+          assets from the design department.
+        </Text>
+        <Text>
+          In a couple hours, we expect everyone will get some of the way through
+          this prototype.{' '}
+          <strong>The prototype is just that — a prototype</strong>. If invited
+          to an on-site interview, you'll pair with us wherever you left off,
+          because we'd much rather see how you work on a problem you're familiar
+          with and have had time to think about than on some clever whiteboard
+          puzzle we spring on you.
+        </Text>
+        <Divider />
+        <Heading as="h4" size="md">
+          Your task
+        </Heading>
+        <Text className="paragraph" fontWeight="bold">
+          Imagine and prototype a fun and delightful pizza configuration screen.
+        </Text>
+        <Heading as="h4" size="md">
+          Guidelines
+        </Heading>
+        <UnorderedList>
+          <ListItem>
+            Try to spend only a few hours on this, but you'll have a week.
+          </ListItem>
+          <ListItem>
+            Just in case you were wondering, we won't use any of your work
+            anywhere, we'll keep it between us.
+          </ListItem>
+          <ListItem>
+            Index on fun and delight, when in doubt, make it more fun. You will
+            be graded on how delightful this is to use. Go wild.
+          </ListItem>
+          <ListItem>
+            It should still be usable — don't be boring…but don't get carried
+            away.
+          </ListItem>
+        </UnorderedList>
+        <Heading as="h4" size="md">
+          Requirements
+        </Heading>
+        <OrderedList>
+          <ListItem>
+            People should be able to choose a size, we offer Small, Medium, and
+            Monster.
+          </ListItem>
+          <ListItem>
+            People should be able to choose their toppings, sorry no ½ or ¼
+            toppings, gotta be the whole pie.
+          </ListItem>
+          <ListItem>
+            People should be able to checkout and start over. Fwiw, you don't
+            have to figure out anything beyond clicking the checkout button (it
+            doesn't have to work).
+          </ListItem>
+        </OrderedList>
+        <Heading as="h4" size="md">
+          Resources
+        </Heading>
+        <UnorderedList>
+          <ListItem>
+            Image assets and colors are provided in the <Code>/public</Code>{' '}
+            folder. You'll find images for pizzas, toppings, etc.{' '}
             <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
+              color="blue.600"
+              textDecoration="underline"
+              isExternal
+              href="https://gamma.app/docs/Pepperoni-Planet-Brand-Guidelines-hndxmj5soxjpf9y?mode=doc"
             >
-              Learn Chakra
+              Check out this gamma for the brand colors, logos and guidelines
+              for Pepperoni Planet.
             </Link>
-          </VStack>
-        </Grid>
-      </Box>
-    </ChakraProvider>
+          </ListItem>
+          <ListItem>
+            The backend engineering team has provided a data-model for you. You
+            can find at <Code>/src/data.js</Code>
+          </ListItem>
+        </UnorderedList>
+        <Heading as="h4" size="md">
+          Slack us
+        </Heading>
+        <Text className="paragraph">
+          We'll be adding you to a Slack channel where the Gamma design team can
+          help you answer any questions or if you get stuck on something. We'll
+          try and be available as much as we can.
+        </Text>
+        <Heading as="h4" size="md">
+          Oh, one last thing...
+        </Heading>
+        <Text className="paragraph" fontStyle="italic">
+          We're looking for people who can take a simple task and make it
+          delightful. We don't expect a robust or feature-complete solution, but
+          we do expect a delightful one. When in doubt, do less, better :).
+        </Text>
+        <Image
+          src="/images/mascot.png"
+          alt="Peppy le Pepperoni"
+          mx="auto"
+          my={12}
+          w="12"
+        />
+      </Stack>
+    </Container>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,356 @@
+export const data = {
+  categories: {
+    menu_items: {
+      name: 'Pizza Bases',
+      description: 'Our cosmic crusts, ready for your stellar toppings',
+      items: [
+        {
+          filename: 'pizza_1.png',
+          title: 'BBQ Orbit Ring',
+          description:
+            'A celestial swirl of BBQ sauce and red onions creating a mesmerizing spiral pattern across our classic crust',
+          ingredients: {
+            sauces: ['sauce_bbq.png'],
+            cheeses: ['cheeze_moz.png'],
+            toppings: ['topping_red_onion.png'],
+          },
+        },
+        {
+          filename: 'pizza_2.png',
+          title: 'Tomato Moon Landing',
+          description:
+            'Fresh sliced tomatoes arranged like lunar craters across a bed of melted cheese and herbs',
+          ingredients: {
+            sauces: ['sauce_marinara.png'],
+            cheeses: ['cheeze_moz.png'],
+            toppings: ['topping_tomato_slices.png'],
+            finishings: ['finishing_basil.png'],
+          },
+        },
+        {
+          filename: 'pizza_3.png',
+          title: 'Jalapeño Meteor Shower',
+          description:
+            'Spicy jalapeños and buffalo sauce creating a fiery constellation across our signature cheese blend',
+          ingredients: {
+            sauces: ['sauce_buffalo.png'],
+            cheeses: ['cheeze_moz.png'],
+            toppings: ['topping_jalepenos.png'],
+          },
+        },
+        {
+          filename: 'pizza_4.png',
+          title: 'Mushroom Meteorites',
+          description:
+            'A galaxy of fresh mushroom slices scattered across our classic marinara and mozzarella base',
+          ingredients: {
+            sauces: ['sauce_marinara.png'],
+            cheeses: ['cheeze_moz.png'],
+            toppings: ['topping_mush.png'],
+          },
+        },
+        {
+          filename: 'pizza_5.png',
+          title: 'Veggie Voyager',
+          description:
+            'An expedition of fresh asparagus, mushrooms, and red peppers charting a course across our artisanal crust',
+          ingredients: {
+            sauces: ['sauce_marinara.png'],
+            cheeses: ['cheeze_moz.png'],
+            toppings: [
+              'topping_mush.png',
+              'topping_roasted_red_pepper.png',
+              'topping_green_pepper.png',
+            ],
+          },
+        },
+        {
+          filename: 'pizza_6.png',
+          title: 'Classic Pepperoni Planet',
+          description:
+            'Our signature pizza with perfectly spaced pepperoni moons orbiting across a sea of melted mozzarella',
+          ingredients: {
+            sauces: ['sauce_marinara.png'],
+            cheeses: ['cheeze_moz.png'],
+            toppings: ['topping_pepperoni.png'],
+          },
+        },
+        {
+          filename: 'pizza_7.png',
+          title: 'Cheese Nebula',
+          description:
+            'A pure cosmic blend of our signature cheeses melted to golden perfection on our classic marinara base',
+          ingredients: {
+            sauces: ['sauce_marinara.png'],
+            cheeses: ['cheeze_moz.png'],
+          },
+        },
+      ],
+    },
+    sauces: {
+      name: 'Space Sauces',
+      description: 'Launch your pizza with our out-of-this-world sauces',
+      items: [
+        {
+          filename: 'sauce_marinara.png',
+          title: 'Classic Marinara',
+          description: 'Our signature red sauce',
+        },
+        {
+          filename: 'sauce_spicy_mar.png',
+          title: 'Spicy Marinara',
+          description: 'Red sauce with an orbital kick',
+        },
+        {
+          filename: 'sauce_alfredo.png',
+          title: 'Cosmic Alfredo',
+          description: 'Creamy white sauce',
+        },
+        {
+          filename: 'sauce_bbq.png',
+          title: 'Celestial BBQ',
+          description: 'Sweet and smoky BBQ sauce',
+        },
+        {
+          filename: 'sause_k_bbq.png',
+          title: 'Korean BBQ',
+          description: 'Sweet and spicy Korean-style BBQ',
+        },
+        {
+          filename: 'sauce_buffalo.png',
+          title: 'Meteor Buffalo',
+          description: 'Classic buffalo sauce',
+        },
+        {
+          filename: 'sauce_pesto.png',
+          title: 'Green Nebula Pesto',
+          description: 'Fresh basil pesto',
+        },
+        {
+          filename: 'sauce_ranch.png',
+          title: 'Ranch Dust',
+          description: 'Creamy ranch sauce base',
+        },
+        {
+          filename: 'sauce_honey_mustard.png',
+          title: 'Solar Honey Mustard',
+          description: 'Sweet and tangy honey mustard',
+        },
+        {
+          filename: 'sauce_chipotle.png',
+          title: 'Chipotle Flare',
+          description: 'Smoky chipotle pepper sauce',
+        },
+      ],
+    },
+    cheeses: {
+      name: 'Celestial Cheeses',
+      description: 'Add some milky way magic with our cheese selection',
+      items: [
+        {
+          filename: 'cheeze_moz.png',
+          title: 'Mozzarella Moon',
+          description: 'Classic shredded mozzarella',
+        },
+        {
+          filename: 'cheese_fat_free_moz.png',
+          title: 'Light Speed Mozzarella',
+          description: 'Fat-free mozzarella option',
+        },
+        {
+          filename: 'cheese_cheddar.png',
+          title: 'Cheddar Crater',
+          description: 'Sharp cheddar cheese',
+        },
+        {
+          filename: 'cheese_parm2.png',
+          title: 'Parmesan Pulsar',
+          description: 'Aged parmesan cheese',
+        },
+        {
+          filename: 'cheeze_gorgon.png',
+          title: 'Gorgonzola Galaxy',
+          description: 'Bold gorgonzola blue cheese',
+        },
+        {
+          filename: 'cheese_crumbles.png',
+          title: 'Feta Falling Star',
+          description: 'Crumbled feta cheese',
+        },
+        {
+          filename: 'cheeze_vegan_moz.png',
+          title: 'Vegan Voyager',
+          description: 'Plant-based mozzarella alternative',
+        },
+      ],
+    },
+    toppings: {
+      name: 'Asteroid Toppings',
+      description:
+        'Customize your cosmic creation with our stellar selection of toppings',
+      items: [
+        {
+          filename: 'topping_pepperoni.png',
+          title: 'Classic Pepperoni',
+          description: 'Our signature pepperoni slices',
+        },
+        {
+          filename: 'topping_sausage.png',
+          title: 'Sausage Supernova',
+          description: 'Italian sausage crumbles',
+        },
+        {
+          filename: 'topping_bacon.png',
+          title: 'Bacon Belt',
+          description: 'Crispy bacon pieces',
+        },
+        {
+          filename: 'topping_can_bacon.png',
+          title: 'Canadian Bacon',
+          description: 'Northern hemisphere ham',
+        },
+        {
+          filename: 'toppping_chicken.png',
+          title: 'Chicken Cluster',
+          description: 'Grilled chicken pieces',
+        },
+        {
+          filename: 'topping_salami.png',
+          title: 'Salami Stars',
+          description: 'Spicy salami slices',
+        },
+        {
+          filename: 'topping_vegan_sausage.png',
+          title: 'Plant-Based Pioneer',
+          description: 'Vegan sausage alternative',
+        },
+        {
+          filename: 'topping_anchovies_yuk.png',
+          title: 'Deep Space Anchovies',
+          description: 'Salty anchovy fillets',
+        },
+        {
+          filename: 'topping_mush.png',
+          title: 'Mushroom Meteorites',
+          description: 'Fresh sliced mushrooms',
+        },
+        {
+          filename: 'topping_green_pepper.png',
+          title: 'Green Pepper Rings',
+          description: 'Fresh bell pepper slices',
+        },
+        {
+          filename: 'topping_roasted_red_pepper.png',
+          title: 'Red Pepper Rockets',
+          description: 'Roasted red peppers',
+        },
+        {
+          filename: 'topping_hot_peppers.png',
+          title: 'Hot Pepper Holes',
+          description: 'Spicy banana peppers',
+        },
+        {
+          filename: 'topping_jalepenos.png',
+          title: 'Jalapeño Jets',
+          description: 'Fresh jalapeño slices',
+        },
+        {
+          filename: 'topping_olives.png',
+          title: 'Black Hole Olives',
+          description: 'Sliced black olives',
+        },
+        {
+          filename: 'topping_red_onion.png',
+          title: 'Red Onion Rings',
+          description: 'Fresh red onion slices',
+        },
+        {
+          filename: 'topping_crushed_garlic.png',
+          title: 'Crushed Garlic Crystals',
+          description: 'Fresh crushed garlic',
+        },
+        {
+          filename: 'topping_roasted_garlic.png',
+          title: 'Roasted Garlic Galaxies',
+          description: 'Sweet roasted garlic',
+        },
+        {
+          filename: 'topping_broc.png',
+          title: 'Broccoli Bursts',
+          description: 'Fresh broccoli florets',
+        },
+        {
+          filename: 'topping_corn.png',
+          title: 'Corn Comets',
+          description: 'Sweet corn kernels',
+        },
+        {
+          filename: 'topping_artichoke.png',
+          title: 'Artichoke Asteroids',
+          description: 'Marinated artichoke hearts',
+        },
+        {
+          filename: 'topping_pineapple.png',
+          title: 'Pineapple Plasma',
+          description: 'Sweet pineapple chunks',
+        },
+        {
+          filename: 'topping_tomatoes.png',
+          title: 'Cherry Tomato Clouds',
+          description: 'Fresh cherry tomatoes',
+        },
+        {
+          filename: 'topping_tomato_slices.png',
+          title: 'Tomato Tides',
+          description: 'Fresh sliced tomatoes',
+        },
+      ],
+    },
+    finishings: {
+      name: 'Final Frontier Finishings',
+      description: 'Complete your cosmic creation with these final touches',
+      items: [
+        {
+          filename: 'finishing_basil.png',
+          title: 'Fresh Basil Flakes',
+          description: 'Fresh basil leaves',
+        },
+        {
+          filename: 'finishing_oregano.png',
+          title: 'Oregano Orbit',
+          description: 'Dried oregano herbs',
+        },
+        {
+          filename: 'topping_rosemary.png',
+          title: 'Rosemary Rain',
+          description: 'Fresh rosemary sprigs',
+        },
+        {
+          filename: 'finishing_arugula.png',
+          title: 'Arugula Aurora',
+          description: 'Fresh arugula leaves',
+        },
+        {
+          filename: 'finishing_spinach.png',
+          title: 'Spinach Satellites',
+          description: 'Fresh baby spinach',
+        },
+        {
+          filename: 'finishing_hot_honey.png',
+          title: 'Hot Honey Horizon',
+          description: 'Spicy honey drizzle',
+        },
+        {
+          filename: 'finishing_ricotta.png',
+          title: 'Ricotta Rainfall',
+          description: 'Fresh ricotta dollops',
+        },
+        {
+          filename: 'finishing_salt_pepper.png',
+          title: 'Cosmic Seasoning',
+          description: 'Classic salt and pepper finish',
+        },
+      ],
+    },
+  },
+};
